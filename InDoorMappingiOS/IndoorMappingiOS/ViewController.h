fb839557b61a6e0f020ab63ec42db596746883c5
Waleed Alotaibi <alotaibi026@macpc1.local>
2015-11-06 12:37:12 -0500

    first commit

---
+++ InDoorMappingiOS/IndoorMappingiOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IndoorMappingiOS
+//
+//  Created by Alotaibi, Waleed N on 11/6/15.
+//  Copyright © 2015 Alotaibi, Waleed N. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ InDoorMappingiOS/IndoorMappingiOS/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  IndoorMappingiOS
+//
+//  Created by Alotaibi, Waleed N on 11/6/15.
+//  Copyright © 2015 Alotaibi, Waleed N. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
