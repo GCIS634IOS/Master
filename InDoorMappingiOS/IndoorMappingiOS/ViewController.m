18326223f5683738ea2c0b39d57edff50b5d4639
Almushigih, Hadeel <almushig001@macpc10.local>
2015-11-16 13:27:28 -0500

    2

--- InDoorMappingiOS/IndoorMappingiOS/ViewController.m
+++ InDoorMappingiOS/IndoorMappingiOS/ViewController.m
@@ -3,7 +3,7 @@
 //  IndoorMappingiOS
 //
 //  Created by Alotaibi, Waleed N on 11/6/15.
-//  Copyright © 2015 Alotaibi, Waleed N. All rights reserved1.
+//  Copyright © 2015 Alotaibi, Waleed N. All rights reserved2.
 //
 
 #import "ViewController.h"
